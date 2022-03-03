dff3e8ab770e68533853fb6c14b3854d31679c50
Your Name <8390ml@gmail.com>
2022-03-03 10:48:09 +0000

    updated variable

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/Iac-MSS/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  variable = resource_group_name
+  resource_group_name
 }
